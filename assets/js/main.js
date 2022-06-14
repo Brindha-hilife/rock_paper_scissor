e3a4d20c7359826e24b91cd513d106a12261aa97
Brindha-hilife <bindu.hilifeai@gmail.com>
2022-06-14 18:38:44 +0530

    Add function for button in index.php

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+function myRock(){
+    document.getElementById('image1')
+        .style.display = "block";
+
+    // document.getElementById('image2')
+    //     .style.display = "none";
+
+    // document.getElementById('image3')
+    //     .style.display = "none";
+}
+ 
+// function myPaper(){
+//     document.getElementById('image2')
+//         .style.display = "block";
+
+//     document.getElementById('image3')
+//         .style.display = "none";
+
+//     document.getElementById('image1')
+//         .style.display = "none";
+// }
+
+// function myScissor(){
+//     document.getElementById('image3')
+//         .style.display = "block";
+
+//     document.getElementById('image1')
+//         .style.display = "none";
+
+//     document.getElementById('image2')
+//         .style.display = "none";
+// }
